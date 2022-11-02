441f1bce4dbb043a7ad271a3711326a6f2028a3b
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-11-02 11:21:08 +0330

    Configure phpunit and testbench

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use MahdiAslami\Laravel\Lang\ServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array<int, string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+}
